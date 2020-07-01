d34f840df08f5bac467b8643a08f3b09d76c6230
Jose Luis Quiroga <joseluisquiroga@yahoo.com>
2020-07-01 17:06:11 -0500

    vpp_lang 3. test2

--- vpp_lang/clean.sh
+++ vpp_lang/clean.sh
@@ -8,5 +8,6 @@ echo "Cleaning" $(basename $SCRIPT_DIR)
 cd $SCRIPT_DIR
 rm -rf src_vpp/peg_output.c
 rm -rf build
+rm -f vpp_lang
 cd $CURR_DIR
 
--- vpp_lang/src_vpp/test_lexis.cpp
+++ vpp_lang/src_vpp/test_lexis.cpp
@@ -1,14 +1,45 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 FILE *input= 0;
 static int   lineNumber= 1;
+
+static char* open_raw = NULL;
+//static char* close_raw = NULL;
 
 #define YY_INPUT(buf, result, max)			\
 {							\
   int c= getc(input);					\
   if ('\n' == c || '\r' == c) ++lineNumber;		\
   result= (EOF == c) ? 0 : (*(buf)= c, 1);		\
+}
+
+
+static int set_open_raw(char* tok){
+	if(tok != NULL){
+		if(open_raw != NULL){
+			free(open_raw);
+			open_raw = NULL;
+		}
+		open_raw = strdup(tok);
+		fprintf(stdout, "Setted open_raw=%s\n", open_raw);
+	}
+	return 1;
+}
+
+static int is_close_raw(char* tok){
+	int ret = 0;
+	if(tok != NULL){
+		if(open_raw != NULL){
+			ret = (strcmp(open_raw, tok) == 0);
+			fprintf(stdout, "Cmp %s==%s ? ret=%d\n", open_raw, tok, ret);
+			free(open_raw);
+			open_raw = NULL;
+		}
+	}
+	return ret;
 }
 
 #include "peg_output.c"
